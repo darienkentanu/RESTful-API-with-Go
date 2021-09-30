593ea4743e1c23e43ecbecf4f55487da61a7fddd
Darien Kentanu <darienkentanu@gmail.com>
2021-09-30 20:18:27 +0700

    commit ke empat

--- controller/controller.go
+++ controller/controller.go
@@ -2,20 +2,15 @@ package controller
 
 import (
 	"net/http"
+	"strconv"
 
+	"github.com/darienkentanu/RESTful-API-with-Go/model"
 	"github.com/labstack/echo"
 )
 
 type M map[string]interface{}
 
-type User struct {
-	Id       int    `json:"id" form:"id"`
-	Name     string `json:"name" form:"name"`
-	Email    string `json:"email" form:"email"`
-	Password string `json:"password" form:"password"`
-}
-
-var users []User
+var users []model.User
 
 // ----------------------- controller --------------------
 
@@ -27,22 +22,78 @@ func GetUsersController(c echo.Context) error {
 	})
 }
 
-func GetUserController(c echo.Echo) error {
+func contains(s []model.User, idx int) bool {
+	for i := range s {
+		if i == idx {
+			return true
+		}
+	}
+	return false
+}
+
+func GetUserController(c echo.Context) error {
 	// your solution here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, M{"messages": "please input a valid routes"})
+	}
+	if contains(users, id-1) {
+		return c.JSON(http.StatusOK, M{
+			"messages": "success get user",
+			"user":     users[id-1],
+		})
+	}
+	return c.JSON(http.StatusBadRequest, M{"message": "id doesn't exist"})
 }
 
 func DeleteUserController(c echo.Context) error {
 	// your solution here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, M{"message": "please input a valid routes"})
+	}
+	if contains(users, id-1) {
+		users = append(users[:id-1], users[id:]...)
+		return c.JSON(http.StatusOK, M{"message": "success to delete user"})
+	}
+	return c.JSON(http.StatusBadRequest, M{"message": "id doesn't exist"})
 }
 
 func UpdateUserControllers(c echo.Context) error {
 	// your solution here
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, M{"message": "please input a valid id"})
+	}
+	if contains(users, id-1) {
+		user := &users[id-1]
+		user.Name = c.FormValue("name")
+		user.Email = c.FormValue("email")
+		user.Password = c.FormValue("password")
+		return c.JSON(http.StatusOK, M{
+			"messages":     "success",
+			"Updated user": user,
+		})
+	}
+	return c.JSON(http.StatusBadRequest, M{"message": "id doesn't exist"})
 }
 
 // create new user
 func CreateUserController(c echo.Context) error {
+	user := model.User{}
+	user.Name = c.FormValue("name")
+	if len(user.Name) == 0 {
+		return c.JSON(http.StatusBadRequest, M{"messages": "please input a valid username"})
+	}
+	user.Email = c.FormValue("email")
+	if len(user.Email) == 0 {
+		return c.JSON(http.StatusBadRequest, M{"messages": "please input a valid email"})
+	}
+	user.Password = c.FormValue("password")
+	if len(user.Password) == 0 {
+		return c.JSON(http.StatusBadRequest, M{"messages": "please input a valid password"})
+	}
 	// binding data
-	user := User{}
 	c.Bind(&user)
 
 	if len(users) == 0 {
--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/darienkentanu/RESTful-API-with-Go
 go 1.16
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/gommon v0.3.0 // indirect
 	github.com/mattn/go-colorable v0.1.11 // indirect
 	github.com/valyala/fasttemplate v1.2.1 // indirect
